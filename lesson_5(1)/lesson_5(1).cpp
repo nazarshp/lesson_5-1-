dc42e576c4b3229d5b1109c11fdf07e3cbcb63a4
Nazar Shpak <nazar.shpak2004@gmail.com>
2022-04-07 22:51:18 +0300

    Add project files.

---
+++ lesson_5(1)/lesson_5(1).cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+void main()
+{
+	int a = 0;
+	std::cout << std::endl;
+	for (int i = 10; i > 0; i--)
+	{
+		std::cout << "\t";
+		for (int j = 0; j < i; j++)
+		{
+			std::cout << ' ';
+		}
+		for (int l = 0; l <= a * 2; l++)
+		{
+			if (i > 1 && i < 10 && (i<l && l<(a*2))  )
+			{
+				 std::cout << " ";
+				
+			}
+	        else
+			{
+				std::cout << "*";
+			}
+		}
+		
+		std::cout << std::endl;
+		
+		a++;
+	}
+}
